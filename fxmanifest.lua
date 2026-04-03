d0b2c20abef55415681448af663203c8e09a6b77
Artmines <arturowells@gmail.com>
2026-04-03 14:30:29 -0230

    Version bump

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 lua54 'yes'
 use_experimental_fxv2_oal 'yes'
 
-version '1.0.0'
+version '1.0.1'
 repository 'https://github.com/Mythic-Framework/mythic-characters'
 
 client_script '@mythic-base/components/cl_error.lua'
